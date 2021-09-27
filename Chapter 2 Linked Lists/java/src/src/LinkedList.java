26971851abb7e2005c7e8f3fa5802106590e0652
bran-ten <bran.ten25@gmail.com>
2021-09-27 19:27:55 -0400

    initial commit

---
+++ Chapter 2 Linked Lists/java/src/src/LinkedList.java
@@ -0,0 +1,69 @@
+public class LinkedList<T> {
+    private Node<T> head;
+    private int size = 0;
+
+    public void addToEnd(T data){
+        if(head == null){
+            head = new Node<>(data);
+            size++;
+            return;
+        }
+        Node<T> current = head;
+        while(current.getNext() != null) {
+            current = current.getNext();
+        }
+        current.setNext(new Node<>(data));
+        size++;
+    }
+
+    public void addToBeginning(T data){
+        Node<T> newHead = new Node<>(data);
+        newHead.setNext(head);
+        head = newHead;
+        size++;
+    }
+
+    public T removeFromEnd(){
+        T data;
+        Node<T> current = head;
+        while(current.getNext() != null){
+            current = current.getNext();
+        }
+        data = current.getData();
+        current = null;
+        size--;
+        return data;
+    }
+
+    public T removeFromBeginning(){
+        T data = head.getData();
+        head = head.getNext();
+        head = null;
+        size--;
+        return data;
+    }
+
+    public void deleteWithValue(T data){
+        if(head == null) return;
+        if(head.getData() == data){
+            head = head.getNext();
+            return;
+        }
+        Node<T> current = head;
+        while(current.getNext() != null){
+            if(current.getNext().getData() == data){
+                current.setNext(current.getNext().getNext());
+                return;
+            }
+            current = current.getNext();
+        }
+    }
+
+    public void removeDuplicates(){
+        Node<T> current = head;
+    }
+
+    public void clear(){
+        head = null;
+    }
+}
---
+++ Chapter 2 Linked Lists/java/src/src/Node.java
@@ -0,0 +1,25 @@
+public class Node<T> {
+    private Node<T> next;
+    private T data;
+
+    public Node(T data){
+        this.data = data;
+        next = null;
+    }
+
+    public Node<T> getNext() {
+        return next;
+    }
+
+    public void setNext(Node<T> next) {
+        this.next = next;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+}
